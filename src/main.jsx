78e652ac7d5f713533b4c6ed933e8d129c3ab40f
aanchalsagitra <aanchalsagitra260@gmail.com>
2024-03-02 22:23:53 +0530

    modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
